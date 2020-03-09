a4bf52a99ff8563d047c70603456e8cdb30ab19b
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-03-09 10:02:39 +0530

    Adding numbers

--- myCalculator.php
+++ myCalculator.php
@@ -6,8 +6,30 @@
     <title>php|calculator</title>
 </head>
 <body>
-    <form action="myCalculator.php" metho="get">
-        <t
+    <form action="myCalculator.php" method="get">
+        <table>
+            <tr>
+                <td>Enter first number:</td>
+                <td><input type="number" name="num1"></td>
+
+            </tr>
+
+            <tr>
+                <td>Enter second number:</td>
+                <td><input type="number" name="num2"></td>
+
+            </tr>
+            <tr>
+                <td>Submit: </td>
+                <td><input type="submit"></td>
+
+            </tr>
+        </table>
+
     </form>
+
+    <?php
+        echo $_GET['num1'] + $_GET['num2'];
+    ?>
 </body>
 </html>
