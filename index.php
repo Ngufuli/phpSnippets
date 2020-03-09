9f78c77443b3d2a1801db6e0fe2373c07d268789
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-03-09 09:40:41 +0530

    form with name

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php|input</title>
+</head>
+<body>
+    <?php
+
+    
+
+    ?>
+
+    <form action="index.php" method="get">
+        <table>
+            <tr>
+                <td>Name: </td>
+                <td><input type="text" name="name"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>Submit</td>
+                <td><input type="submit"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
