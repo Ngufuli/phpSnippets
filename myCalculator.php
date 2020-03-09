0be2adcc554fa63c97732bbe54008e5848fab7f9
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-03-09 09:59:05 +0530

    calculator

---
+++ myCalculator.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php|calculator</title>
+</head>
+<body>
+    <form action="myCalculator.php" metho="get">
+        <t
+    </form>
+</body>
+</html>
