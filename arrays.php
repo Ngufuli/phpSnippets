b72fa8712f261c0b4e8c1e45b5e640f45fd3d85f
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-03-09 17:17:20 +0530

    arrays

---
+++ arrays.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php|arrays</title>
+</head>
+<body>
+    <?php
+
+         $cars = array('lamborghini', 'bugatti', 'volkswagen','Hammer', 'Ferrari');
+
+         echo $cars[1];
+
+    ?>
+</body>
+</html>
