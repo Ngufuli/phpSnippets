cc256f9d807aa9262c66e958a51e4ad84b6886d9
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-03-09 10:52:35 +0530

    simple madlibs game

---
+++ madLibsGame.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>madLibsGame|php</title>
+</head>
+<body>
+    <form action="madLibsGame.php" action="post">
+        <table>
+            <tr>
+                <td>Color: </td>
+                <td></td>
+            </tr>
+
+            <tr>
+                <td>Plural Noun: </td>
+                <td></td>
+            </tr>
+
+            <tr>
+                <td>Celebrity</td>
+                <td></td>
+            </tr>
+        </table>
+    </form>
+    <br>
+
+    <?php
+    
+    ?>
+</body>
+</html>
