25086a47adcdd34a0605d6f546a6d154ac4a6c3d
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-03-10 13:15:12 +0530

    with negation

--- if.php
+++ if.php
@@ -9,13 +9,20 @@
     
 
     <?php
-    $isHuman = true;
+    $isHuman = false;
+    $isEarth = false;
 
-    if($isHuman){
-        echo "<center><h1><pre>He is a human being</pre></h1></center>";
+    if($isHuman && $isEarth){
+        echo "<center><h1><pre>He is a human being from planet Earth</pre></h1></center>";
     }
-    else{
-        echo "<center><h1><pre>He is not a Human being</pre></h1></center>";
+    else if(!$isHuman && $isEarth){
+        echo "<center><h1><pre>He is not a Human being but he is from planet Earth</pre></h1></center>";
+    }
+    else if($isHuman && !$Earth){
+        echo "<center><h1><pre>He is a human being but not from planet Earth</pre></h1></center>";
+    }
+    else {
+        echo "<center><h1><pre>He is neither a human being nor a creature from planet Earth, there-fore he is an Interplanetary Alien.</pre></h1><center>";
     }
 
     ?>
