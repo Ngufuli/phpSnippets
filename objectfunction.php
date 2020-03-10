09e902a98d71a5c07e7d98c69445aa89f4cf9f15
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-03-10 16:12:17 +0530

    minor changes

---
+++ objectfunction.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>function|objects</title>
+</head>
+<body>
+    <?php
+        class Student{
+            var $name;
+            var $course;
+            var $gpa;
+
+            function __construct($n, $c, $g){
+                $this->name = $n;
+                $this->course = $c;
+                $this->gpa = $g;
+            }
+
+            function hasHonors(){
+                if(this->gpa >= 3.5){
+                    echo "The student has honors";
+                }
+                else{
+                    echo "The student doesn't have honors";
+                }
+            }
+        }
+
+        $Student1 = new Student('Zam',"Business", 3.5);
+        $Student2 = new Student("Zim", "Arts", 3.4);
+
+        e
+    ?>
+</body>
+</html>
