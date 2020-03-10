bfed7e877e65b22164a3662a93a6e07313357eb6
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-03-10 15:42:36 +0530

    constructor

--- constructors.php
+++ constructors.php
@@ -9,7 +9,37 @@
     
 
     <?php
-        
+    //function which is called when we create an object of a class
+    class Novel{
+        var $title;
+        var $author;
+        var $pages;
+
+        function __construct(){
+            echo "New novel created<br/>";
+        }
+    }
+
+
+   $novel1 = new Novel;
+    $novel1->title="The beginnig";
+    $novel1->author="Nick";
+    $novel1->pages=400;
+
+
+
+
+   $novel2 = new Novel;
+    $novel2->title="The ending";
+    $novel2->author="Nickolson";
+    $novel2->pages=440;
+
+
+    echo $novel1->title;
+    echo "<br>";
+    echo $novel2->title;
+
+
     ?>
 </body>
 </html>
