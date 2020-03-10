9b03d599cab727f12ab5d9fe2580a38d1b76299d
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-03-10 15:20:57 +0530

    site php file

---
+++ site.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site</title>
+</head>
+<body>
+    <?php 
+        $title = "My first Post";
+        $author = "Nick";
+        $wordcount = 400;
+
+        include 'articleHeader.php';
+    ?>
+</body>
+</html>
