a65e5f3747fd23d71840e5a28d4c0724f05c6cb8
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-03-10 14:48:50 +0530

    while loop

---
+++ while.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>while|phpLoops</title>
+</head>
+<body>
+    
+
+    <?php
+        $index = 1;
+        while($index<=5){
+            echo "<center><h1><pre>Hellow world $index</pre><h1></center>";
+            $index++;
+        }
+    ?>
+</body>
+</html>
