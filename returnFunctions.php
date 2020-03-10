e44cc7185d4544b1dec7e3dff302a7e69faa1d1c
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-03-10 12:45:57 +0530

    with return

---
+++ returnFunctions.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>return|php</title>
+</head>
+<body>
+    
+
+
+    <?php
+        function cube($number){
+            $value = pow($number, 3);
+            return $value;
+        }
+
+        $answer = cube(5);
+
+        echo "<center><h1><pre>$answer</pre></h1></center>";
+    ?>
+</body>
+</html>
