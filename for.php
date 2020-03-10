e38c3ab0dddeb6bfd439d2bffd7093abe47a3a95
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-03-10 14:56:29 +0530

    for loops

---
+++ for.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>for|phpLoops</title>
+</head>
+<body>
+    
+
+
+
+    <?php
+        for($i =1; $i<=10; $i++){
+            echo "<center><h1><pre>$i</pre></h1></center>";
+            echo "<br><hr>";
+        }
+    ?>
+</body>
+</html>
