a1f6ffb99f22ac2ffb743e0df0269c16ede49338
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-03-10 14:15:29 +0530

    switch

---
+++ switch.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>switch|php</title>
+</head>
+<body>
+    <form action="switch.php" method="post">
+        <table>
+            <tr>
+                <td>Enter your grade: </td>
+                <td><input type="text" name="grade"></td>
+            </tr>
+            <tr>
+                <td>Submit: </td>
+                <td><input type="submit"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
