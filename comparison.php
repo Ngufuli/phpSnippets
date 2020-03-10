26cf41a6c3b5bb04ee7dab01f35e39033f648ac7
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-03-10 13:33:48 +0530

    max

--- comparison.php
+++ comparison.php
@@ -43,7 +43,7 @@
             if($num1>=$num2 && $num1>=$num3){
                 return $num1;
             }
-            else if($num2>$num3){
+            else if($num2>$num3 && $num2>$num1){
                 return $num2;
             }
             else{
