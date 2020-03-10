7dfbb8fa64ead29bf7e8ac781e83f4a61c705378
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-03-10 15:02:31 +0530

    for loop

--- for.php
+++ for.php
@@ -11,10 +11,17 @@
 
 
     <?php
-        for($i =1; $i<=10; $i++){
-            echo "<center><h1><pre>$i</pre></h1></center>";
-            echo "<br><hr>";
-        }
+    //     for($i =1; $i<=10; $i++){
+    //         echo "<center><h1><pre>$i</pre></h1></center>";
+    //         echo "<br><hr>";
+    //     }
+
+    //
+            $arr = array(1,2,3,4,5,4,3,3,2,1);
+            for ($i=1; $i<= count($arr); $i++){
+                echo "<center><pre>$arr[5]<br/></pre></center>";
+            }
     ?>
+
 </body>
 </html>
