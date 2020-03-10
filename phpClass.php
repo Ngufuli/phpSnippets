cd271939a2137989df7a67d4928bbec14b24083a
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-03-10 15:27:07 +0530

    classes

---
+++ phpClass.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php|class</title>
+</head>
+<body>
+    <?php
+        class Novel{
+            //Title, Author and Number of pages
+            //Above will be the attributes
+            //Novel will be the datatype
+
+            var $title; 
+            var $author;
+            var $pages;
+        }
+    ?>
+</body>
+</html>
