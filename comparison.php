b6692dba22bae03c88999bdfab46e7c318e469db
Nicodemus Ngufuli <ngufuli@outlook.com>
2020-03-10 13:24:08 +0530

    comparison

---
+++ comparison.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>comparison|php</title>
+</head>
+<body>
+    <form action="comparison.php" method="post">
+
+        <table>
+            <tr>
+                <td>Enter the first number: </td>
+                <td><input type="number" name="num1"></td>
+            </tr>
+
+            <tr>
+                <td>Enter the second number: </td>
+                <td><input type="number" name="num2"></td>
+            </tr>
+
+            <tr>
+                <td>Enter the third number: </td>
+                <td><input type="number" name="num3"></td>
+            </tr>
+
+            <tr>
+                <td>Submit</td>
+                <td><input type="submit"></td>
+            </tr>
+        </table>
+    
+    </form>
+</body>
+</html>
